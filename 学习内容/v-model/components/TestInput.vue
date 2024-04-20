9272efda32f0bf866d023bb16578da58b17520e3
lyao <qy_MCiq7hNah2MscxAsg>
2024-04-20 12:12:43 +0800

    v-model学习

---
+++ 学习内容/v-model/components/TestInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">
+    <input type="text" :value="modelValue" @input="emit('update:modelValue',$event.target.value)">
+
+  </div>
+</template>
+<script setup lang="ts" name="TestInput">
+defineProps(['modelValue'])
+const emit = defineEmits(['update:modelValue'])
+</script>
+<style scoped>
+.test input{
+  width: 200px;
+  border-radius: 10px;
+}
+</style>
---
+++ 学习内容/v-model/components/TestInput2.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test">
+    <input type="text" :value="modelValue" @input="emit('update:modelValue',$event.target.value)">
+    <input type="text" :value="m2value" @input="emit('update:m2value',$event.target.value)">
+
+  </div>
+</template>
+<script setup lang="ts" name="TestInput">
+defineProps(['modelValue','m2value'])
+const emit = defineEmits(['update:modelValue','update:m2value'])
+</script>
+<style scoped>
+.test input{
+  width: 200px;
+  border-radius: 10px;
+}
+</style>
